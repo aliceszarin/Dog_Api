abbafddfd1a166b6f8132d99d694b6648d68dc02
Alice Szarin <124408030+aliceszarin@users.noreply.github.com>
2023-05-25 22:30:57 -0300

    Update dog_api.py

--- dog_api.py
+++ dog_api.py
@@ -1,13 +1,8 @@
-# Consumir mais uma API publica:
-        
+# Consumir uma API publica de dogs, que retorna um status HTTP:     
 import requests 
 
 url_base = 'https://http.dog/'
 endpoint = f' {url_base}/500.json'
-
-
 resposta = requests.get(endpoint)
-
 dog_api = resposta.json()
-
 print ('Olha que legal esse dog: ', dog_api)
